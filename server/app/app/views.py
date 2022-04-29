ea3338c5e067e2e342a872791a052f7f40f17d8a
Andrew Silkwood <silkwood.andrew@gmail.com>
2022-04-29 12:18:40 -0700

    fix windows issue with upper and lowercase

--- server/app/app/views.py
+++ server/app/app/views.py
@@ -11,6 +11,7 @@ from cv2 import imwrite, imread
 from shutil import rmtree
 import bmark
 
+
 @app.route('/', methods=['GET'])
 @devEnvironment
 def index():
@@ -22,7 +23,8 @@ def index():
                            title='Image To Font',
                            base_fonts=web_utils.base_font_list(),
                            templates=web_utils.template_dict(),
-    )
+                           )
+
 
 @app.route('/process', methods=['POST'])
 @requires_auth
@@ -31,12 +33,12 @@ def process():
     if request.method != 'POST':
         flash('Invalid request', 'danger')
         abort(400, "Invalid request. Please use POST.")
-    
+
     # Check for image in request (body) and retrieve
     if 'image' not in request.files:
         flash('No image in upload', 'danger')
         abort(400, "No image in upload")
-    
+
     # Get the image and image details
     image = request.files['image']
     filename = secure_filename(image.filename)
@@ -49,7 +51,8 @@ def process():
     internal_name = f"{fontname}-{uuid4().hex}"
 
     # Retrieve Template Type
-    templateType = secure_filename(request.form.get('template_type', "english_lower_case_letters"))
+    templateType = secure_filename(request.form.get(
+        'template_type', "english_lower_case_letters"))
     templateType = os.path.splitext(templateType)[0] + ".csv"
     if templateType not in web_utils.template_dict():
         flash('Invalid template type', 'warning')
@@ -58,34 +61,36 @@ def process():
 
     # Retrieve Base Font
     base_font = secure_filename(
-            request.form.get("base_font", app.config['DEFAULT_BASE_FONT'])
+        request.form.get("base_font", app.config['DEFAULT_BASE_FONT'])
     )
     if os.path.exists(os.path.join(app.config["FONTS_FOLDER3"], base_font)):
         print(f"Using '{base_font}' for missing characters.")
     else:
-        flash(f'The base font \'{request.form["base_font"]}\' does not exist. Using the default base font \'{base_font}\' instead.', 'warning')
-        print(f'The base font \'{request.form["base_font"]}\' does not exist. Using the default base font \'{base_font}\' instead.')
+        flash(
+            f'The base font \'{request.form["base_font"]}\' does not exist. Using the default base font \'{base_font}\' instead.', 'warning')
+        print(
+            f'The base font \'{request.form["base_font"]}\' does not exist. Using the default base font \'{base_font}\' instead.')
         base_font = app.config['DEFAULT_BASE_FONT']
 
     # Check that the image has an appropriate name
     if not allowed_image(filename):
         flash('Invalid image type', 'warning')
         abort(400, "Invalid image type: %s" % filename)
-    
+
     # Check that the image is smaller than the maximum allowed size
     image.seek(0, os.SEEK_END)
     size = image.tell()
     if not allowed_image_filesize(size):
         flash('Image too large', 'warning')
         abort(400, "Image too large: %s" % filename)
-    
+
     # Upload Image
-    clean(app.config['IMAGE_UPLOADS']) # Clean up old images
-    clean(app.config['PROCESSED_IMAGES']) # Clean up old images
+    clean(app.config['IMAGE_UPLOADS'])  # Clean up old images
+    clean(app.config['PROCESSED_IMAGES'])  # Clean up old images
     upload_filepath = os.path.join(
-        app.config['IMAGE_UPLOADS'], internal_name + ext) # Get upload filepath
-    image.seek(0) # Reset file pointer
-    image.save(upload_filepath) # Save image to filepath
+        app.config['IMAGE_UPLOADS'], internal_name + ext)  # Get upload filepath
+    image.seek(0)  # Reset file pointer
+    image.save(upload_filepath)  # Save image to filepath
     flash('Image uploaded successfully', 'success')
 
     # Process Image, clean image
@@ -94,7 +99,7 @@ def process():
     clean(app.config['CROPPED_IMAGES'])
     imwrite(os.path.join(
         app.config['CROPPED_IMAGES'], internal_name + ext), cropped_image)
-    processed_image = bmark.image.process(cropped_image, 300) 
+    processed_image = bmark.image.process(cropped_image, 300)
     clean(app.config['PROCESSED_IMAGES'])
     imwrite(os.path.join(
         app.config['PROCESSED_IMAGES'], internal_name + ext), processed_image)
@@ -113,7 +118,7 @@ def process():
 
     # Cut image. cutImages is a tuple (cut_images, flattened_template)
     # cut_images_tuple = cut_image(imread(upload_filepath), processed_image, templateType, processed_image_tuple[1])
-    cuttable_image = bmark.image.process(cropped_image, 1500) 
+    cuttable_image = bmark.image.process(cropped_image, 1500)
     [cut_images, flattened_template] = bmark.image.cut(
         cuttable_image, processed_image, template)
     cut_image_path = os.path.join(
@@ -129,14 +134,17 @@ def process():
             if cutImage.size == 0:
                 flash("Grid estimation error, check output", "warning")
             else:
-                imwrite(os.path.join(cut_image_path, symbol + ".jpg"), cutImage)
-                imwrite(os.path.join(unboxed_image_path, symbol + ".jpg"), bmark.image.unbox(cutImage))
+                imwrite(os.path.join(
+                    cut_image_path, f"{symbol}{1 if symbol.isupper() else 2}.jpg"), cutImage)
+                imwrite(os.path.join(
+                    unboxed_image_path, f"{symbol}{1 if symbol.isupper() else 2}.jpg"), bmark.image.unbox(cutImage))
     flash('Image cut successfully', 'success')
 
     # Convert cut images into svgs
     # Convert SVGs into a font
     svg_path = os.path.join(app.config['SVG_IMAGES'], internal_name)
-    font_path = os.path.join(app.config['FONTS_FOLDER2'], internal_name + ".otf")
+    font_path = os.path.join(
+        app.config['FONTS_FOLDER2'], internal_name + ".otf")
     clean(svg_path)
     bmark.font.create(unboxed_image_path, svg_path, font_path)
 
@@ -145,14 +153,15 @@ def process():
     assert os.path.exists(base_font_path)
     bmark.font.merge_font(font_path, base_font_path, output_file=font_path)
 
-    return Response("{'status':'success','filename':'"+ internal_name + ".otf" +"'}", status=201)
+    return Response("{'status':'success','filename':'" + internal_name + ".otf" + "'}", status=201)
+
 
 @app.route('/identify_character', methods=['POST'])
 @requires_auth
 def identify_character():
     if request.method != 'POST':
         abort(400, "Invalid request. Please use POST.")
-    
+
     # Check if the post request has an image
     if 'image' not in request.files:
         abort(400, "No image in upload")
@@ -168,13 +177,13 @@ def identify_character():
     # Check that the image has an appropriate name
     if not allowed_image(filename):
         abort(400, "Invalid image type: %s" % filename)
-    
+
     # Check that the image is smaller than the maximum allowed size
     image.seek(0, os.SEEK_END)
     size = image.tell()
     if not allowed_image_filesize(size):
         abort(400, "Image too large: %s" % filename)
-    
+
     # Upload Image
     filename = secure_filename(image.filename)
     clean(app.config['IMAGE_UPLOADS'])
@@ -186,7 +195,8 @@ def identify_character():
 
     # Identify character
     text = bmark.ml.image_to_text(upload_filepath)
-    return Response("{'status':'success','character':'"+ text +"'}", status=200)
+    return Response("{'status':'success','character':'" + text + "'}", status=200)
+
 
 @app.route('/font/<path:filename>')
 @requires_auth
@@ -196,15 +206,18 @@ def font(filename):
         filename += '.otf'
     return send_from_directory(app.config['FONTS_FOLDER'], secure_filename(filename), as_attachment=True)
 
+
 @app.route('/base-fonts', methods=['GET'])
 @requires_auth
 def base_font_list():
     return jsonify(web_utils.base_font_list())
 
+
 @app.route('/templates', methods=['GET'])
 @requires_auth
 def template_dict():
     return jsonify(web_utils.template_dict())
+
 
 @app.route('/render-template/<path:template_name>')
 @requires_auth
@@ -216,9 +229,11 @@ def create_template(template_name):
     template = web_utils.load_template(template_name)
     height = len(template)
     width = len(template[0]) if template else 0
-    image = bmark.template.create(width,height, 50, 5)
-    image.save(os.path.join("app", app.config["TEMPLATE_IMAGES_FOLDER"], template_name + ".png"))
+    image = bmark.template.create(width, height, 50, 5)
+    image.save(os.path.join(
+        "app", app.config["TEMPLATE_IMAGES_FOLDER"], template_name + ".png"))
     return send_from_directory(app.config["TEMPLATE_IMAGES_FOLDER"], template_name + ".png", as_attachment=True)
+
 
 @app.route('/preview')
 @devEnvironment
@@ -234,6 +249,7 @@ def preview():
         if f[0] in get_font_list():
             return render_template('public/preview.html', title='Font Preview', args=f[0])
     return render_template('public/preview.html', title='Font Preview', args='AmogusFont.ttf')
+
 
 def clean(path):
     """
